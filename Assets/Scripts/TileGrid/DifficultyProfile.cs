e66ee900f0ffa3f6c83dceb43dd90cc8a9506dff
JohnathanGlasgow <glasgjg1@student.op.ac.nz>
2024-04-27 16:06:02 +1200

    refactor: add shuffle method to difficulty profile and switch to class

--- Assets/Scripts/TileGrid/DifficultyProfile.cs
+++ Assets/Scripts/TileGrid/DifficultyProfile.cs
@@ -1,11 +1,12 @@
 /*
- * File: TileData.cs
- * Purpose: Defines structs used for tile generation
+ * File: DifficultyProfile.cs
+ * Purpose: Defines a class and helper structs used for tile generation
  * Author: Johnathan
  * Contributions: Assisted by GitHub Copilot
  */
 
 using System.Collections.Generic;
+using UnityEngine;
 
 /// <summary>
 /// A struct containing a tile data and its probability
@@ -15,7 +16,7 @@ using System.Collections.Generic;
 public struct TileProbability
 {
     public TileData tileData;   // tile data for this tile
-    public float probability;   // probability of this tile (should be between 0 and 100)
+    public float probability;   // probability of this tile (should be between 0 and 100, anything above 100 is effectively 100% and anything below 0 is effectively 0%)
 }
 
 /// <summary>
@@ -29,11 +30,34 @@ public struct RowSetting
 }
 
 /// <summary>
-/// A struct containing the tile probabilities and the amounts of the various rows
+/// A class containing the tile probabilities and the amounts of the various rows
 /// This is used by TileGridChunk.GenerateTileGrid to generate a chunk of tiles
+/// </summary>
 [System.Serializable]
-public struct DifficultyProfile
+public class DifficultyProfile
 {
     public List<TileProbability> tileProbabilities;
     public List<RowSetting> rowSettings;
+
+    public void ShuffleProbabilities()
+    {
+        Shuffle(tileProbabilities);
+        Shuffle(rowSettings);
+    }
+
+    /// <summary>
+    /// Shuffle a list with the Fisher-Yates algorithm
+    /// </summary>
+    /// <typeparam name="T">The type of the list</typeparam>
+    /// <param name="list">The list to shuffle</param>
+    private void Shuffle<T>(List<T> list)
+    {
+        for (int i = 0; i < list.Count; i++)
+        {
+            int randomIndex = Random.Range(i, list.Count);
+            T temp = list[i];
+            list[i] = list[randomIndex];
+            list[randomIndex] = temp;
+        }
+    }
 }
