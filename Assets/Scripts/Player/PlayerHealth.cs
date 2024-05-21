52e8002478b2336fc94d862405ba7978f8185caf
DevonPartridgeOfficer <partda1@student.op.ac.nz>
2024-05-21 19:11:32 +1200

    ADD: Comments in PlayerHealth script

--- Assets/Scripts/Player/PlayerHealth.cs
+++ Assets/Scripts/Player/PlayerHealth.cs
@@ -13,17 +13,31 @@ public class PlayerHealth : MonoBehaviour
     private const int startingHP = 1;
     private int currentHP;
 
-    [Header("Player Clothing bools")]
+    [Header("Player Clothing bools")] //Public so it can be modified by ClothingPickup script
     public bool hatWorn;
     public bool shirtWorn;
     public bool pantsWorn;
 
+    /// <summary>
+    /// Set the initial values of all variables
+    /// </summary>
     void Start()
     {
         SetValuesToDefaults();
     }
-    
-    void SetValuesToDefaults()
+
+    /// <summary>
+    /// Update the player health value during gameplay
+    /// </summary>
+    private void Update()
+    {
+        CheckHealth();
+    }
+
+    /// <summary>
+    /// Set the initial values of all variables in Start and Reset
+    /// </summary>
+    public void SetValuesToDefaults()
     {
         currentHP = startingHP; //Set the health back to 1
         hatWorn = false;
@@ -31,11 +45,10 @@ public class PlayerHealth : MonoBehaviour
         pantsWorn = false;
     }
 
-    private void Update()
-    {
-        CheckHealth();
-    }
 
+    /// <summary>
+    /// Updates health during gameplay depending on clothes worn 
+    /// </summary>
     void CheckHealth()
     {
         int healthIncrease = 0; //Value increases for each worn item of clothing
@@ -44,8 +57,6 @@ public class PlayerHealth : MonoBehaviour
         if (shirtWorn) { healthIncrease++; }
         if (pantsWorn) { healthIncrease++;  }
         
-        currentHP = startingHP + healthIncrease;
-        Debug.Log($"Current Health: {currentHP}");
-        Debug.Log($"Health Increase: {healthIncrease}");
+        currentHP = startingHP + healthIncrease; //Limit health to a max of 4
     }
 }
